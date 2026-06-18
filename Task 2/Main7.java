84a53a7e75661ba16ad0ee597fe4248271984a8b
Ahsan Ahmed Saif <151661036+Saif724@users.noreply.github.com>
2026-06-18 16:05:28 +0600

    Update Main7.java

--- Task 2/Main7.java
+++ Task 2/Main7.java
@@ -1,1 +1,16 @@
 
+interface clickable {
+    void onClick();
+}
+class Button implements clickable {
+    public void onClick(){
+        System.out.println("Button is clicked!");
+    }
+    
+}
+public class Main7{
+    public static void main(String[] args){
+        Button b=new Button();
+        b.onClick();
+    }
+}
