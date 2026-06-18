2d32e484f18e269f21e6ec55d8f0e608e3befd27
Ahsan Ahmed Saif <151661036+Saif724@users.noreply.github.com>
2026-06-18 16:03:43 +0600

    Update Main1.java

--- Task 2/Main1.java
+++ Task 2/Main1.java
@@ -1,1 +1,12 @@
-
+class ContestStates{
+    int a[]={85,67,87};
+    void printFirstScores(){
+        System.out.println(a[0]);
+    }
+}
+public class Main1{
+    public static void main(String[] args){
+        ContestStates ct=new ContestStates();
+        ct.printFirstScores();
+    }
+}
