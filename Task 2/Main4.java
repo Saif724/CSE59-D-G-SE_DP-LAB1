555d1dc7ba339bd58ed431baddf76acebea2798e
Ahsan Ahmed Saif <151661036+Saif724@users.noreply.github.com>
2026-06-18 16:04:40 +0600

    Update Main4.java

--- Task 2/Main4.java
+++ Task 2/Main4.java
@@ -1,1 +1,15 @@
 
+class Sensor{
+    String  name;
+    int batteryLevel;
+    void charge(){
+        batteryLevel=100;
+        System.out.println("Sensor fully Charged!");
+    }
+}
+public class Main4{
+    public static void main(String[] args){
+        Sensor s=new Sensor();
+        s.charge();
+    }
+}
