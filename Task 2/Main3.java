66f88ac0209515874e35b9b34ddbcf8c1f2e0b9d
Ahsan Ahmed Saif <151661036+Saif724@users.noreply.github.com>
2026-06-18 16:04:25 +0600

    Update Main3.java

--- Task 2/Main3.java
+++ Task 2/Main3.java
@@ -1,1 +1,15 @@
 
+class CountDown{
+    void start(){
+        for(int i=5; i>0; i--){
+            System.out.println(i+" ");
+        }
+        System.out.println("Go!");
+    }
+}
+public class Main3{
+    public static void main(String[] args){
+        CountDown cd=new CountDown();
+        cd.start();
+    }
+}
