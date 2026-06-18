cda1b6a51688cbfb0bf0ee957737c9a5c0580f80
Ahsan Ahmed Saif <151661036+Saif724@users.noreply.github.com>
2026-06-18 16:04:55 +0600

    Update Main5.java

--- Task 2/Main5.java
+++ Task 2/Main5.java
@@ -1,1 +1,16 @@
 
+class Printer{
+    void printData(String s){
+        System.out.println(s);
+    }
+    void printData(int num){
+        System.out.println(num);
+    }
+}
+public class Main5{
+    public static void main(String[] args){
+        Printer p=new Printer();
+        p.printData("fjdklsj");
+        p.printData(348957);
+    }
+}
