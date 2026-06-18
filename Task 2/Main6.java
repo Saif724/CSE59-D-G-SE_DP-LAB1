a463fb7e3b724ac7d711178d4c3424843ad088f9
Ahsan Ahmed Saif <151661036+Saif724@users.noreply.github.com>
2026-06-18 16:05:10 +0600

    Update Main6.java

--- Task 2/Main6.java
+++ Task 2/Main6.java
@@ -1,1 +1,18 @@
 
+class Applet{
+    void launch(){
+        System.out.println("Applet launched!");
+    }
+}
+class GameApplet extends Applet {
+    void play(){
+        System.out.println("Playing Game!");
+    }
+}
+public class Main6{
+    public static void main(String[] args){
+        GameApplet ga=new GameApplet();
+        ga.play();
+        ga.launch();
+    }
+}
